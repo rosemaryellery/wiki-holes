f5cd39956b1d505c9db4a211feac704ed24d90d8
rosemaryellery <98529835+rosemaryellery@users.noreply.github.com>
2022-09-11 17:24:18 +0100

    Updates

---
+++ precache-manifest.db409fecdaaba4f0557063a685e03bed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c75e4f98e7af42df3340",
+    "url": "/wiki-holes/static/css/main.9b1c9ce1.chunk.css"
+  },
+  {
+    "revision": "c75e4f98e7af42df3340",
+    "url": "/wiki-holes/static/js/main.cba2e7d0.chunk.js"
+  },
+  {
+    "revision": "d49307e1a98e6d97bb0b",
+    "url": "/wiki-holes/static/js/runtime~main.af7448c0.js"
+  },
+  {
+    "revision": "4b33aed58be7df9dde3f",
+    "url": "/wiki-holes/static/js/2.f3017430.chunk.js"
+  },
+  {
+    "revision": "c758845bced192807ae6b7e69c07113b",
+    "url": "/wiki-holes/index.html"
+  }
+];
